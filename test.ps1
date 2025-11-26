e8a5c60e01dd4c7ae5af38cfe399de8935f252c5
Jacob Hummer <jcbhmr@outlook.com>
2025-11-26 14:17:43 -0600

    Add PowerShell script for Windows architecture check

---
+++ test.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/env pwsh
+$ErrorActionPreference = "Stop"
+
+if (-not $IsWindows) {
+  Write-Host "This script only works on Windows systems"
+  Exit 1
+}
+
+$architecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
+$expected = ""
+if ($architecture -eq "X64") {
+  $expected = "x86_64-pc-windows-msvc"
+}
+if ($architecture -eq "ARM64") {
+  $expected = "arm64-pc-windows-msvc"
+}
+$actual = &"./config.guess"
+
+if ($actual -ne $expected) {
+  Write-Host "expected $expected, got $actual"
+  Exit 1
+}
+
+Write-Host "got expected $expected"
